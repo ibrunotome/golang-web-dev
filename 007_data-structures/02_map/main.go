357d4be3bc2770947d2ea75ce5c6c8c5c0483b59
Bruno Tomé <ibrunotome@gmail.com>
2018-06-26 09:32:31 -0300

    Passing composite data structures into templates

---
+++ 007_data-structures/02_map/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.html"))
+}
+
+func main() {
+	// sages := []string{"Gandhi", "MLK", "Buddha", "Jesus", "Muhammad"}
+
+	sages := map[string]string{
+		"India":    "Gandhi",
+		"America":  "MLK",
+		"Meditate": "Buddha",
+		"Love":     "Jesus",
+		"Prophet":  "Muhammad",
+	}
+
+	err := tpl.Execute(os.Stdout, sages)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
